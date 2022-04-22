2c79c4ca71b0c6250ca13dff52f80ebc47a69677
MuhammadZaldy <104199660+MuhammadZaldy@users.noreply.github.com>
2022-04-22 18:23:57 +0700

    Add files via upload

---
+++ muhammad zaldy triadmaja - 191011402114 - 06tplm002 -UTS kecerdasan buatan (if else).py
@@ -0,0 +1,31 @@
+# If dan Else Statement
+
+# 1. if nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama anda? ")
+
+# program sebelumnya
+# if kondisi: aksi
+# program selanjutnya
+
+# 1. program if inline
+# if nama=="zaldy" : print("akses diterima!!!!")
+# print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+# if nama=="zaldy":
+# 	print("kamu tampan!")
+# 	print("kamu juga tampan banget!")
+# print(f"Terima kasih {nama}")
+
+# 3. Else Statement
+
+if nama=="zaldy":
+	print("hai zaldy, akses diterima!!!")
+else:
+	print("kamu bukan zaldy, akses ditolak! :(")
+
+print("akhir dari program")
